0c2e7e80c9afe8424494a8cb51916667cb146743
Andrew Leung <1062708758@qq.com>
2022-11-14 19:32:13 +0800

    init project

---
+++ app/src/main/java/com/soku/a24danalysiser/MainActivity.java
@@ -0,0 +1,14 @@
+package com.soku.a24danalysiser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
